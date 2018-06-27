6a3beca656c32f99bd8a6d9b0fe06080e47ad105
minorbread <hmcdl@foxmail.com>
2018-06-27 20:42:36 +0800

    branch Cnode api proxy

--- client/config/router.jsx
+++ client/config/router.jsx
@@ -3,9 +3,11 @@ import { Route, Redirect } from 'react-router-dom';
 
 import TopicList from '../views/topic-list/index';
 import TopicDetail from '../views/topic-detail/index';
+import TestApi from '../views/test/api-test'
 
 export default () => [
   <Route path="/" key="index" render={() => <Redirect to="/list" />} exact />,
   <Route path="/list" key="list" component={TopicList} />,
   <Route path="/detail" key="detail" component={TopicDetail} />,
+  <Route path="/test" component={TestApi} exact key="test" />,
 ]
--- server/server.js
+++ server/server.js
@@ -3,10 +3,30 @@ const ReactDomServer = require('react-dom/server')
 const favicon = require('serve-favicon')
 const fs = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const app = express()
 
 const isDev = process.env.NODE_ENV === 'development'
+
+// application/json 解析
+app.use(bodyParser.json())
+//  application/x-www-form-urlencoded 解析
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
+
+app.use(session({
+  maxAge: 10 * 60 * 1000,
+  name: 'tid',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'react demo'
+}))
+
+
+app.use('/api/user', require('./util/handle-login'))
+app.use('/api', require('./util/proxy'))
 
 if (!isDev) {
   const serverEntry = require('../dist/server-entry').default
@@ -25,7 +45,6 @@ if (!isDev) {
   devStatic(app)
 }
 
-app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
 app.listen(3000, () => {
   console.log('success 3000')
